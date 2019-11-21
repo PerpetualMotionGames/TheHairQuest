fd2798071bf6d51cdcdc6e87ca3384683d4a9c37
KatieLG <katie.louise.gardner@gmail.com>
2019-11-21 22:38:29 +0000

    Added a preview ability
    Simplified tileSwitch code - didn't need the ability to swap between more than two tilemaps for this jam, have also added an ability to preview the other reality without swapping them by holding shift - have made sure the audio isn't affected by that

--- Assets/Scripts/TileSwitch.cs
+++ Assets/Scripts/TileSwitch.cs
@@ -8,50 +8,49 @@ using UnityEngine.SceneManagement;
 public class TileSwitch : MonoBehaviour
 {
 	public Tilemap[] tileStates; // for the two maps of the game world
-
 	private int activeTileSet = 0;
 
 	public PostProcessVolume vol;
 	LensDistortion distortion;
-	private bool canSwap = true;
+	private bool canSwap = true; //only allow a world shift if this is true
+	private bool isShifting = false; // this keeps track of whether the player is previewing the other state
 
-	public float swapTransitionTime = 0.25f;
+	public float swapTransitionTime = 0.25f; //how long does it take to do the swapping transition
 	public float maxLensDistort = 30f;
 	public float minAlpha = 0.2f; //transparency of the inactive layer
+
+	public float frontAlph = 1;
+	public float backAlph = 0.2f;
 
     void Start()
     {
 		UpdateTileCollider();
-		SetAlpha(tileStates[0], 1);
-		SetAlpha(tileStates[1], 0.2f);
+		SetAlpha(frontAlph, backAlph);
 		vol.profile.TryGetSettings(out distortion);
     }
 
-	public void SetAlpha(Tilemap map, float alpha)
+	public void TileAlpha(Tilemap map, float alpha) //for a specific tilemap set the alpha of the colour
 	{
 		Color mapCol = map.color;
 		mapCol.a = alpha;
 		map.color = mapCol;
 	}
-	public void InstantSwapAlpha()
+	public void SetAlpha(float foregroundAlpha, float backGroundAlpha)  //set the alpha value of the active tileset and the inactive one.
 	{
-		SetAlpha(tileStates[0], tileStates[0].color.a == 1 ? 0.2f : 1);
-		SetAlpha(tileStates[1], tileStates[1].color.a == 1 ? 0.2f : 1);
+		TileAlpha(tileStates[activeTileSet], foregroundAlpha);
+		TileAlpha(tileStates[1-activeTileSet], backGroundAlpha);
 	}
+
 	public void SwitchTileState()
 	{
-		activeTileSet += 1;
-		activeTileSet = activeTileSet % tileStates.Length; //could do activeTileSet = 1-activeTileset but this way if we did ever go to having more than 2 states this is better
+		activeTileSet = 1 - activeTileSet; 
 		StartCoroutine(SwapEffect());
 	}
 
 	private void UpdateTileCollider()
 	{
-		for (int i = 0; i < tileStates.Length; i++)
-		{
-			bool current = activeTileSet == i;
-			tileStates[i].gameObject.GetComponent<TilemapCollider2D>().enabled = current;
-		}
+		tileStates[activeTileSet].gameObject.GetComponent<TilemapCollider2D>().enabled = true;
+		tileStates[1-activeTileSet].gameObject.GetComponent<TilemapCollider2D>().enabled = false;
 	}
 
 	private bool LegalSwap()
@@ -61,7 +60,6 @@ public class TileSwitch : MonoBehaviour
 		{
 			Vector3Int belowPoint = tileStates[activeTileSet].WorldToCell(transform.position + Vector3.down / 2);
 			string name = tileStates[activeTileSet].GetTile(belowPoint).name;
-			Debug.Log(name);
 			if(name=="jungleTilemap_1" || name == "jungleTilemap_4"){
 				return true;
 			}
@@ -80,11 +78,12 @@ public class TileSwitch : MonoBehaviour
 		}
 		if (Input.GetKeyDown(KeyCode.LeftShift))
 		{
-			InstantSwapAlpha();
+			isShifting = true;
+			SetAlpha(backAlph, frontAlph);
 		}
-		if (Input.GetKeyUp(KeyCode.LeftShift))
+		if (Input.GetKeyUp(KeyCode.LeftShift) && isShifting)
 		{
-			InstantSwapAlpha();
+			SetAlpha(frontAlph, backAlph);
 		}
 		if (Input.GetKeyDown(KeyCode.R))
 		{
@@ -111,19 +110,18 @@ public class TileSwitch : MonoBehaviour
 
 	IEnumerator SwapEffect()
 	{
-		var alph = 1f;
+		isShifting = false;
+		var alph = frontAlph;
 		UpdateTileCollider();
 		canSwap = false;
 		while ( distortion.intensity.value > -maxLensDistort)
 		{
 			distortion.intensity.value -=  maxLensDistort * Time.deltaTime / swapTransitionTime;
-			alph -= (1-minAlpha) * Time.deltaTime / (swapTransitionTime);
-			SetAlpha(tileStates[1-activeTileSet], alph);
-			SetAlpha(tileStates[activeTileSet], 1.2f - alph);
+			alph -= (1 - minAlpha) * Time.deltaTime / (swapTransitionTime);
+			SetAlpha(1 + minAlpha - alph,alph);
 			yield return new WaitForEndOfFrame();
 		}
-		SetAlpha(tileStates[1 - activeTileSet], .2f);
-		SetAlpha(tileStates[activeTileSet], 1);
+		SetAlpha(frontAlph, backAlph);
 
 		while (distortion.intensity.value < 0)
 		{
