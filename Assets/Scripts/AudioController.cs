093b60e75a411fe051144b1145a7db37367922b8
TJSteel <jaysteel78@msn.com>
2019-11-24 22:09:15 +0000

    updated the audio controller
    adapted the audio switch file which is now the audio controller
    this has the option to play all of the sound effects we have
    
    set the player so that they can emmit the jump sound and also playerHit
    if falling from higher than their double jump height

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource soundtrackA, soundtrackB;
+    private static AudioSource boxCollision, gameOver, jump, lava, playerHit, pushBox, splash, water;
+    TileSwitch tileswap;
+    public float vol;
+
+    private int audioState = 0;
+    // Start is called before the first frame update
+    void Start() {
+        tileswap = GameObject.Find("Player").GetComponent<TileSwitch>();
+
+        boxCollision = GameObject.Find("BoxCollision").GetComponent<AudioSource>();
+        gameOver = GameObject.Find("GameOver").GetComponent<AudioSource>();
+        jump = GameObject.Find("Jump").GetComponent<AudioSource>();
+        lava = GameObject.Find("Lava").GetComponent<AudioSource>();
+        playerHit = GameObject.Find("PlayerHit").GetComponent<AudioSource>();
+        pushBox = GameObject.Find("PushBox").GetComponent<AudioSource>();
+        splash = GameObject.Find("Splash").GetComponent<AudioSource>();
+        water = GameObject.Find("Water").GetComponent<AudioSource>();
+
+        soundtrackB.volume = 0;
+        soundtrackA.volume = vol;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void SwitchSound() {
+        audioState = 1 - audioState;
+        StartCoroutine(AudioTransitionEnumerator());
+    }
+
+    IEnumerator AudioTransitionEnumerator() {
+        float transitionTime = tileswap.swapTransitionTime;
+
+        AudioSource playing = audioState == 0 ? soundtrackA : soundtrackB;
+        AudioSource muted = audioState == 0 ? soundtrackB : soundtrackA;
+
+        while (muted.volume > 0.05f) {
+            playing.volume += vol * Time.deltaTime / transitionTime;
+            muted.volume -= vol * Time.deltaTime / transitionTime;
+            yield return new WaitForEndOfFrame();
+        }
+        playing.volume = vol;
+        muted.volume = 0;
+    }
+
+    public static void PlaySound(string clip) {
+        switch (clip) {
+            case "BoxCollision":
+                boxCollision.Play();
+                break;
+            case "GameOver":
+                gameOver.Play();
+                break;
+            case "Jump":
+                jump.Play();
+                break;
+            case "Lava":
+                lava.Play();
+                break;
+            case "PlayerHit":
+                playerHit.Play();
+                break;
+            case "PushBox":
+                pushBox.Play();
+                break;
+            case "Splash":
+                splash.Play();
+                break;
+            case "Water":
+                water.Play();
+                break;
+        }
+    }
+
+}
