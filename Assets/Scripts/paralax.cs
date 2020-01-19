58bfdcea2fd742b2cfb6a6975c5744dc14a7aad0
KatieLG <katie.louise.gardner@gmail.com>
2020-01-19 09:47:19 +0000

    Quick parallax hack

---
+++ Assets/Scripts/paralax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paralax : MonoBehaviour
+{
+	Vector3 startPos;
+	GameObject target;
+	Vector3 mystart;
+	public float modifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+		target = GameObject.Find("Main Camera");
+		startPos = target.transform.position;
+		mystart = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = mystart - (startPos - target.transform.position)*modifier;
+    }
+}
