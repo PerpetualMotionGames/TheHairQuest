22b824ecaf47b1326523ce2a635efbf43fa1dedf
Jay Steel <35134286+TJSteel@users.noreply.github.com>
2020-02-04 20:21:37 +0000

    Revert "Finished Background Controller"

---
+++ Assets/Scripts/CompleteLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteLevel : MonoBehaviour
+{
+	// Start is called before the first frame update
+    void Start()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		PlayerSupplement playcontroller = GameObject.Find("Player").GetComponent<PlayerSupplement>();
+		SceneLoader controller = playcontroller.sceneLoader;
+		if (collision.gameObject.tag == "Player")
+		{
+			controller.SetSceneComplete();
+			controller.LoadNextLevel();
+		}
+	}
+}
