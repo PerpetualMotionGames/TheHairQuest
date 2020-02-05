6f3bc276725c2c8bb4d5a91234ee1583d0b942c1
TJSteel <jaysteel78@msn.com>
2020-02-05 00:03:05 +0000

    merged player controller and supplement

--- Assets/Scripts/Object Scripts/CompleteLevel.cs
+++ Assets/Scripts/Object Scripts/CompleteLevel.cs
@@ -18,8 +18,8 @@ public class CompleteLevel : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		PlayerSupplement playcontroller = GameObject.Find("Player").GetComponent<PlayerSupplement>();
-		SceneLoader controller = playcontroller.sceneLoader;
+		PlayerController playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+		SceneLoader controller = playerController.sceneLoader;
 		if (collision.gameObject.tag == "Player")
 		{
 			controller.SetSceneComplete();
